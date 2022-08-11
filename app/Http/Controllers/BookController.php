53b7fdd8d6f1995c05064f48eaf08909fe3e22e4
VA <54583234+vickyarisandy@users.noreply.github.com>
2022-08-11 11:47:21 +0700

    validasi

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -9,7 +9,12 @@ class BookController extends Controller
 {
     public function create(Request $request)
     {
-        $data = $request->all();
+        // $data = $request->all();
+        $data =  $this->validate($request, [
+            "name" => "required|unique:book",
+            "price" => "required",
+            "status" => "required"
+        ]);
         $book = Book::create($data);
 
         return response()->json($book);
